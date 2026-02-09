cbd1a610263e1a2fae7cc9c7acc7c794b22bef7a
Himanshu <ht5620085@gmail.com>
2026-02-09 20:32:06 +0530

    db.js file complete

--- db.js
+++ db.js
@@ -2,4 +2,18 @@ const { MongoClient } = require('mongodb');
 
 const URI = "mongodb://localhost:27017/";
 
-const client = new MongoClient()
+const client = new MongoClient(URI);
+
+let db;
+
+async function getDB() {
+    if (db) return db;
+
+    await client.connect();
+    db = client.db('test');
+
+    console.log('MongDB Connected');
+    return db;
+}
+
+module.exports = getDB;

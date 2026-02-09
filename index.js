5f225d5fc59f4f1edffc9f8779185d8925502780
Himanshu <ht5620085@gmail.com>
2026-02-09 21:22:23 +0530

    adding routes

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 
+// route
+const users = require('./routes/users')
+
 const app = express();
 
 const PORT = 8080;
 
 app.use(express.json());
+
+app.route("/users", users);
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send("Server error");
+})
 
 app.listen(PORT);
